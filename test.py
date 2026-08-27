9e657fe03a0703a2f35dc4935964191684f8dd44
Thejas BJ <thejasbj666@gmail.com>
2026-08-27 11:30:29 +0530

    push

---
+++ test.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number : "))
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i>j: 
+            print(j,end=" ")
+        else:
+            print(i,end=" ")
+    print()
